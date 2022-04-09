9d295005e4f62e882ae772b49141536c11e9aecf
Carlos Diestro <cardiest@gmail.com>
2022-04-09 12:19:29 +0200

    Add project scaffolding

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Heading } from '@chakra-ui/react';
+
+const Home = () => {
+  return (
+    <Heading textAlign="center" my={8}>
+      Be welcome
+    </Heading>
+  );
+};
+
+export default Home;
