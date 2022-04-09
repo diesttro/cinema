55033b73a56363c202a16d4a935467ba6805add0
Carlos Diestro <cardiest@gmail.com>
2022-04-09 16:35:06 +0200

    Add trending movies api call

---
+++ pages/api/trending.js
@@ -0,0 +1,14 @@
+const handler = async (request, response) => {
+  try {
+    const moviesResponse = await fetch(
+      `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`
+    );
+    const movies = await moviesResponse.json();
+
+    response.status(200).json({ trending: movies?.results });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default handler;
