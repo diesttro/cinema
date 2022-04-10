4c7992326063e4fe3da119bfe8a97ad5b3b8c7f5
Carlos Diestro <cardiest@gmail.com>
2022-04-10 20:15:16 +0200

    Show popular movies

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 import { Container, Heading, Text, Box, Tag } from '@chakra-ui/react';
 import { getTrending } from './api/trending';
+import { getPopular } from './api/popular';
 import { getGenres } from './api/genres';
 import MovieSlider from '../components/MovieSlider';
 
-const Home = ({ trending, genres }) => {
+const Home = ({ trending, genres, popular }) => {
   return (
     <Container maxW="container.xl" py={8}>
       <Heading as="h1" size="2xl" py={8}>
@@ -18,27 +19,34 @@ const Home = ({ trending, genres }) => {
         </Heading>
         <MovieSlider items={trending} />
       </Box>
-      <Box my={12}>
+      <Box mt={12} mb={-4}>
         <Heading size="lg" py={8}>
           Genres
         </Heading>
         <Box>
           {genres.map((genre) => (
-            <Tag key={genre.id} mr={4} mb={4} px={4} py={2}>
+            <Tag key={genre.id} mb={4} mr={4} px={4} py={2}>
               {genre.name}
             </Tag>
           ))}
         </Box>
+      </Box>
+      <Box my={12}>
+        <Heading size="lg" py={8}>
+          Popular
+        </Heading>
+        <MovieSlider items={popular} />
       </Box>
     </Container>
   );
 };
 
 const getStaticProps = async (context) => {
-  const [trending, genres] = await Promise.allSettled([getTrending(), getGenres()]);
+  const results = await Promise.allSettled([getTrending(), getGenres(), getPopular()]);
+  const [trending, genres, popular] = results.map((result) => result.value);
 
   return {
-    props: { trending: trending.value, genres: genres.value },
+    props: { trending, genres, popular },
   };
 };
 
