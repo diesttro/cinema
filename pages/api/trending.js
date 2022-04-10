6260aae70bdfb67aa308e81d4624c8ca186b0f0a
Carlos Diestro <cardiest@gmail.com>
2022-04-10 12:50:49 +0200

    Split trending movies api call

--- pages/api/trending.js
+++ pages/api/trending.js
@@ -1,14 +1,21 @@
+const getTrending = async () => {
+  const dataResponse = await fetch(
+    `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`
+  );
+  const data = await dataResponse.json();
+
+  return data?.results;
+};
+
 const handler = async (request, response) => {
   try {
-    const moviesResponse = await fetch(
-      `https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.API_KEY}`
-    );
-    const movies = await moviesResponse.json();
+    const data = await getTrending();
 
-    response.status(200).json({ data: movies?.results });
+    response.status(200).json({ data });
   } catch (error) {
     console.log(error);
   }
 };
 
 export default handler;
+export { getTrending };
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,9 @@
-import { useState, useEffect } from 'react';
-import { Container, Heading, Text, Box } from '@chakra-ui/react';
+import { Container, Heading, Text, Box, Tag } from '@chakra-ui/react';
+import { getTrending } from './api/trending';
+import { getGenres } from './api/genres';
 import MovieSlider from '../components/MovieSlider';
 
-const Home = () => {
-  const [trending, setTrending] = useState([]);
-
-  const fetchTrending = async () => {
-    const response = await fetch('api/trending');
-    const result = await response.json();
-
-    return result?.data;
-  };
-
-  useEffect(() => {
-    fetchTrending().then(setTrending);
-  }, []);
-
+const Home = ({ trending, genres }) => {
   return (
     <Container maxW="container.xl" py={8}>
       <Heading as="h1" size="2xl" py={8}>
@@ -30,8 +18,29 @@ const Home = () => {
         </Heading>
         <MovieSlider items={trending} />
       </Box>
+      <Box my={12}>
+        <Heading size="lg" py={8}>
+          Genres
+        </Heading>
+        <Box>
+          {genres.map((genre) => (
+            <Tag key={genre.id} mr={4} mb={4} px={4} py={2}>
+              {genre.name}
+            </Tag>
+          ))}
+        </Box>
+      </Box>
     </Container>
   );
 };
 
+const getStaticProps = async (context) => {
+  const [trending, genres] = await Promise.allSettled([getTrending(), getGenres()]);
+
+  return {
+    props: { trending: trending.value, genres: genres.value },
+  };
+};
+
 export default Home;
+export { getStaticProps };
