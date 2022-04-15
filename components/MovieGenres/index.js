565d88b4d4d30fa3e0b23b52b3780aea8026cb6b
Carlos Diestro <cardiest@gmail.com>
2022-04-15 12:04:23 +0200

    Refactor movie genres

--- components/MovieGenres/index.js
+++ components/MovieGenres/index.js
@@ -1,12 +1,14 @@
-import Link from 'next/link';
-import { Tag, Link as Anchor } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Tag, TagLabel, Link } from '@chakra-ui/react';
 
 const MovieGenres = ({ items }) => {
   return items.map((item) => (
     <Tag key={item.id} mb={4} mr={4} px={4} py={2}>
-      <Link href={`/genre/${item.id}`} passHref>
-        <Anchor>{item.name}</Anchor>
-      </Link>
+      <NextLink href={`/genre/${item.id}`} passHref>
+        <Link>
+          <TagLabel>{item.name}</TagLabel>
+        </Link>
+      </NextLink>
     </Tag>
   ));
 };
