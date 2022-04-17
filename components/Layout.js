c3d9adf833b7f0072f0ee64177c6062e745540c9
Carlos Diestro <cardiest@gmail.com>
2022-04-17 16:50:35 +0200

    Add link to logo

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,13 @@
-import { Container, Heading } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Container, Heading, Link } from '@chakra-ui/react';
 
 const Layout = ({ children }) => {
   return (
     <Container maxW="container.xl" py={8}>
       <Heading as="h1" size="2xl" py={8}>
-        Cinema
+        <NextLink href="/" passHref>
+          <Link _hover={{ textDecoration: 'none' }}>Cinema</Link>
+        </NextLink>
       </Heading>
       {children}
     </Container>
