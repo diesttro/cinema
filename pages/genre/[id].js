de993aa102b1c949bb4a823da133c3f661db6d27
Carlos Diestro <cardiest@gmail.com>
2022-04-18 15:15:34 +0200

    Change data fetching to client

--- pages/genre/[id].js
+++ pages/genre/[id].js
@@ -1,35 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { SimpleGrid, Box, Link } from '@chakra-ui/react';
-import { getGenre } from '../api/genre/[id]';
+import { SimpleGrid, Box, Link, Skeleton } from '@chakra-ui/react';
 
-const Movies = ({ items }) => {
+const Movies = () => {
+  const { query } = useRouter();
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const getGenreMovies = async () => {
+      const response = await fetch(`/api/genre/${query.id}`);
+      const result = await response.json();
+
+      setItems(result?.data);
+    };
+
+    if (query?.id) getGenreMovies();
+  }, [query]);
+
   return (
-    <SimpleGrid columns={[2, 3, 4]} gap={[0, 1, 2]} mt={12}>
-      {items.map((item) => (
-        <Box key={item.id} mr={4} mb={4}>
-          <NextLink href={`/movie/${item.id}`} passHref>
-            <Link display="flex" borderRadius="2xl" overflow="hidden">
-              <Image
-                alt={item.title}
-                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                width="300px"
-                height="450px"
-              />
-            </Link>
-          </NextLink>
-        </Box>
-      ))}
+    <SimpleGrid columns={[2, 3, 4]} gap={[2, 4, 6]} mt={12}>
+      {items.length
+        ? items.map((item) => (
+            <Box key={item.id}>
+              <NextLink href={`/movie/${item.id}`} passHref>
+                <Link display="flex" borderRadius="2xl" overflow="hidden">
+                  <Image
+                    alt={item.title}
+                    src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                    width="300px"
+                    height="450px"
+                  />
+                </Link>
+              </NextLink>
+            </Box>
+          ))
+        : Array(8)
+            .fill()
+            .map((_, index) => (
+              <Skeleton key={index} maxWidth="100%" pt="150%" borderRadius="2xl" />
+            ))}
     </SimpleGrid>
   );
 };
 
-const getServerSideProps = async (context) => {
-  const { params } = context;
-  const items = await getGenre(params.id);
-
-  return { props: { items } };
-};
-
 export default Movies;
-export { getServerSideProps };
