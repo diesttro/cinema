deba13edd3ee95f81486e8d18d8ea1f3f0753184
Carlos Diestro <cardiest@gmail.com>
2022-04-18 15:14:52 +0200

    Delete slider skeleton

--- components/Movies/Slider/Slider.js
+++ components/Movies/Slider/Slider.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { Skeleton, Link } from '@chakra-ui/react';
+import { Link } from '@chakra-ui/react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
 import 'swiper/css';
@@ -28,28 +28,20 @@ const Slider = ({ items, controls }) => {
         nextEl: controls?.next ?? controls,
       }}
     >
-      {items.length
-        ? items.map((item) => (
-            <SwiperSlide key={item.id}>
-              <NextLink href={`/movie/${item.id}`} passHref>
-                <Link display="flex" borderRadius="2xl" overflow="hidden">
-                  <Image
-                    alt={item.title}
-                    src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
-                    width="300px"
-                    height="450px"
-                  />
-                </Link>
-              </NextLink>
-            </SwiperSlide>
-          ))
-        : Array(5)
-            .fill()
-            .map((_, index) => (
-              <SwiperSlide key={index}>
-                <Skeleton maxW="100%" pt="150%" borderRadius="2xl" />
-              </SwiperSlide>
-            ))}
+      {items.map((item) => (
+        <SwiperSlide key={item.id}>
+          <NextLink href={`/movie/${item.id}`} passHref>
+            <Link display="flex" borderRadius="2xl" overflow="hidden">
+              <Image
+                alt={item.title}
+                src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+                width="300px"
+                height="450px"
+              />
+            </Link>
+          </NextLink>
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
