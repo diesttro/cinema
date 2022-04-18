a8fbd6a6bed9a438249b4304a9da214d6c06e47f
Carlos Diestro <cardiest@gmail.com>
2022-04-18 12:16:19 +0200

    Add movie detail page

---
+++ pages/movie/[id].js
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import { Heading, SimpleGrid, Center, Text, Box } from '@chakra-ui/react';
+import { getMovie } from '../api/movie/[id]';
+
+const Movie = ({ item }) => {
+  return (
+    <SimpleGrid
+      columns={1}
+      sx={{
+        '@media only screen and (min-width: 50rem)': {
+          gridTemplateColumns: 'minmax(0, 300px) 1fr',
+          gridGap: '2rem',
+        },
+      }}
+    >
+      <Center>
+        <Box display="flex" borderRadius="2xl" overflow="hidden">
+          <Image
+            alt={item.title}
+            src={`https://image.tmdb.org/t/p/w500${item.poster_path}`}
+            width="300px"
+            height="450px"
+          />
+        </Box>
+      </Center>
+      <Box>
+        <Heading size="lg" py={8}>
+          {item.title}
+        </Heading>
+        <Text>{item.overview}</Text>
+      </Box>
+    </SimpleGrid>
+  );
+};
+
+const getServerSideProps = async (context) => {
+  const { params } = context;
+  const item = await getMovie(params.id);
+
+  return { props: { item } };
+};
+
+export default Movie;
+export { getServerSideProps };
