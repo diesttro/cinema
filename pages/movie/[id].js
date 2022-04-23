6b8a16f1f9adb7c0829d8ac6ddc5ce783715f20c
Carlos Diestro <cardiest@gmail.com>
2022-04-23 11:41:07 +0200

    Delete production countries

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -13,13 +13,10 @@ const formatRuntime = (hours, minutes) => {
   return runtiemString.trim();
 };
 
-const formatCountries = (countries) => countries?.map((country) => country.iso_3166_1).join(', ');
-
 const Movie = ({ item }) => {
   const [hours, minutes] = timeFromMinutes(item.runtime);
   const hasRuntime = hours > 0 || minutes > 0;
   const releaseYear = splitDateString(item.release_date).year;
-  const countries = formatCountries(item.production_countries);
 
   return (
     <>
@@ -57,7 +54,6 @@ const Movie = ({ item }) => {
               </Text>
             )}
             {releaseYear && <Text mr={4}>{releaseYear}</Text>}
-            {countries.length && <Text>{countries}</Text>}
           </Box>
           <Box my={4}>
             <Genres items={item.genres} />
